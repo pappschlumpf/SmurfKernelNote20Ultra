7fed06ed4cd6ec0abb23299c4df55c5bcb2e53d4
Erik Müller <erik.lars.mueller@googlemail.com>
2020-10-12 22:20:45 +0200

    sdfat remove debug

--- fs/sdfat/blkdev.c
+++ fs/sdfat/blkdev.c
@@ -96,7 +96,6 @@ s32 bdev_check_bdi_valid(struct super_block *sb)
 			fsi->prev_eio |= SDFAT_EIO_BDI;
 			sdfat_log_msg(sb, KERN_ERR, "%s: block device is "
 				"eliminated.(bdi:%p)", __func__, sb->s_bdi);
-			sdfat_debug_warn_on(1);
 		}
 		return -ENXIO;
 	}
@@ -159,7 +158,6 @@ s32 bdev_mread(struct super_block *sb, u64 secno, struct buffer_head **bh, u64 n
 	if (!(fsi->prev_eio & SDFAT_EIO_READ)) {
 		fsi->prev_eio |= SDFAT_EIO_READ;
 		sdfat_log_msg(sb, KERN_ERR, "%s: No bh. I/O error.", __func__);
-		sdfat_debug_warn_on(1);
 	}
 
 	return -EIO;
@@ -213,7 +211,6 @@ no_bh:
 	if (!(fsi->prev_eio & SDFAT_EIO_WRITE)) {
 		fsi->prev_eio |= SDFAT_EIO_WRITE;
 		sdfat_log_msg(sb, KERN_ERR, "%s: No bh. I/O error.", __func__);
-		sdfat_debug_warn_on(1);
 	}
 
 	return -EIO;
